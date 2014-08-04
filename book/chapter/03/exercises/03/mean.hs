bbf7b56fe5d18fe403b8934faf5be9b316b23bd7
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2014-08-04 22:17:15 +0200

    Calculating the mean

--- book/chapter/03/exercises/03/mean.hs
+++ book/chapter/03/exercises/03/mean.hs
@@ -1,2 +1,9 @@
 -- Write a function that computes the mean of a list, i.e., the sum of all elements in the list divided by its length.
 -- (You may need to use the fromIntegral function to convert the length of the list from an integer into a floating-point number.)
+
+theSum :: [Double] -> Double
+theSum []                 =  0.0
+theSum (element:elements) = element + theSum elements
+
+theMean :: [Double] -> Double
+theMean elements = (theSum elements) / fromIntegral( length(elements))
