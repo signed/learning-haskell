0d90ad6b385ef411d5b1dc4494e536fedce78a90
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2014-08-04 22:04:27 +0200

    Added a function signature

--- book/chapter/03/exercises/02/lengthWithTypeSignature.hs
+++ book/chapter/03/exercises/02/lengthWithTypeSignature.hs
@@ -1,2 +1,5 @@
--- Add a type signature for your function to yoursourcefile.
+-- Add a type signature for your function to your sourcefile.
 -- To test it, load the source file into ghci again.
+theLength :: [a] -> Int
+theLength (element:elements) = 1 + theLength elements
+theLength []                 = 0
