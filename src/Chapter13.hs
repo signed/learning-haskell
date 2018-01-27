55616d47f186cb67ea372b816a69f9b5d8e797ba
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2018-01-27 17:07:05 +0100

    chapter 13

--- src/Chapter13.hs
+++ src/Chapter13.hs
@@ -1,1 +1,11 @@
 module Chapter13 where
+
+import Control.Monad
+import System.Exit
+
+palindrome :: IO ()
+palindrome = forever $ do
+  line1 <- getLine
+  case (line1 == reverse line1) of
+    True -> putStrLn "It's a palindrome!"
+    False -> exitSuccess
