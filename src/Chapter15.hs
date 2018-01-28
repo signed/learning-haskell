2ac4ac7b4f29cf9661b0a00d46064a2752b6b3a0
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2018-01-28 20:38:26 +0100

    chapter 15

--- src/Chapter15.hs
+++ src/Chapter15.hs
@@ -1,1 +1,58 @@
 module Chapter15 where
+
+import Data.Maybe
+import Data.Monoid
+
+data Optional a = Nada | Only a deriving (Eq, Show)
+
+-- p 914
+instance Monoid a => Monoid (Optional a) where
+  mempty = Nada
+  Nada `mappend` Nada = Nada
+  Only v `mappend` Nada = Only v
+  Nada `mappend` Only w = Only w
+  (Only v) `mappend` (Only w) = Only (v `mappend` w)
+
+
+
+-- Mad Lips p 924
+type Verb = String
+type Adjective = String
+type Adverb = String
+type Noun = String
+type Exclamation = String
+madlibbin' :: Exclamation
+           -> Adverb
+           -> Noun
+           -> Adjective
+           -> String
+madlibbin' e adv noun   adj =
+ e <> "! he said " <> adv <> " as he jumped into his car " <> noun <> " and drove off with his " <> adj <> " wife."
+
+
+madlibbinBetter' :: Exclamation
+                 -> Adverb
+                 -> Noun
+                 -> Adjective
+                 -> String
+madlibbinBetter' e adv noun adj =  mconcat [e ,"! he said ", adv, " as he jumped into his car ", noun , " and drove off with his ",adj, " wife."]
+
+-- p 934 Maybe another monoid
+
+newtype First' a = First' { getFirst' :: Optional a } deriving (Eq, Show)
+
+instance Monoid (First' a) where
+ mempty = First' Nada
+ mappend (First' (Only l)) _ = First' (Only l)
+ mappend (First' Nada) r = r
+
+firstMappend :: First' a -> First' a -> First' a
+firstMappend = mappend
+
+type FirstMappend =
+     First' String
+  -> First' String
+  -> First' String
+  -> Bool
+type FstId =
+  First' String -> Bool
--- test/Chapter15Spec.hs
+++ test/Chapter15Spec.hs
@@ -1,12 +1,52 @@
 module Chapter15Spec where
 
 import Test.Hspec
+import Test.Hspec.Checkers
+import Test.QuickCheck
+import Test.QuickCheck.Classes
+import Test.QuickCheck.Checkers
 import Chapter15
+import Data.Monoid(Sum)
+
+--
+--main :: IO ()
+--main = 'hspec' spec
+--
+--spec :: Spec
+--spec = do
+--  'testBatch' ('monoid' (undefined :: [Int]))
+
+optionalGenerator :: Arbitrary a => Gen (Optional a)
+optionalGenerator = do
+  v <- arbitrary
+  elements [Nada, Only v]
+
+instance Arbitrary a => Arbitrary (Optional a) where
+  arbitrary = optionalGenerator
+
+instance Eq a => EqProp (Optional a) where
+  (=-=) = eq
+
+firstGenerator :: Arbitrary a =>  Gen(First' a)
+firstGenerator = do
+   variant <- arbitrary
+   frequency [(99,return (First' variant)), (1,return (First' Nada))]
+
+instance Arbitrary a => Arbitrary (First' a) where
+  arbitrary = firstGenerator
+
+instance Eq a => EqProp (First' a) where
+  (=-=) = eq
 
 spec :: Spec
-spec =
+spec = do
   describe "Two Integers" $ do
     it "is equal if both integers match" $
       1 `shouldBe` 1
     it "is not equal if the first integers do not match" $
       2 `shouldBe` 2
+
+  testBatch (monoid (undefined :: [Int]))
+  testBatch (monoid (undefined :: (Optional (Sum Int))))
+  testBatch (monoid (undefined :: (First' String)))
+
